8c1000ae6caca1a048c3642249610a3b6d4d3e65
YashDjsonDookun <dookun88@gmail.com>
2021-04-05 11:27:55 +0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask import render_template
+from flask import request
+import requests
+from dotenv import dotenv_values
+import json
+
+app = Flask(__name__)
+
+token = dotenv_values(".env")["TOKEN"]
+BASE_URL = "https://superheroapi.com/api/"+token
+
+dataFetched = [{"results":""}]
+
+@app.route('/')
+def index():
+    return render_template('index.html', len=len(dataFetched), dataFetched=dataFetched)
+
+@app.route('/search', methods=['POST'])
+def getData():
+    super_hero = request.form['name']
+    print(super_hero)
+    if (len(super_hero)<1):
+        super_hero_dropdown = request.form['names']
+        response = requests.get(BASE_URL+"/search/" + super_hero_dropdown)
+    else:
+        response = requests.get(BASE_URL+"/search/" + super_hero)
+    print(response)
+    data = response.json()
+    temp = []
+    temp.append(data)
+    print(data)
+    globals()['dataFetched'] = temp[0]["results"];
+    return data
+
+@app.route('/names')
+def getNames():
+    with open("names.json") as names:
+        data = names.read()
+    return data
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+$(function(){
+	$('#btn-getData').click(function(e){
+        e.preventDefault();
+        let name_inputBox = $('#name').val();
+        let name_dropdown = $('#names option:selected').text();
+
+		$.ajax({
+			url: '/search',
+			data: $('form').serialize(),
+			type: 'POST',
+			success: function(response){
+                $('#name').val("");
+                $('#names').prop('selectedIndex',0);
+				console.log(response);
+                $("#app").load(location.href + " #app");
+			},
+			error: function(error){
+				console.log(error);
+			}
+		});
+	});
+});
